a8028f6638439046a688d17b8e3dda9a19a0d2c4
Leah Hanson <astrieanna@gmail.com>
2012-03-30 17:18:50 -0400

    lots of stuff....currently works

--- simpleserver.hs
+++ simpleserver.hs
@@ -2,9 +2,12 @@ import Data.Bits
 import Network.Socket
 import Network.BSD
 import Data.List
-import Control.Monad.State
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.RWS.Lazy
+import Data.Set (Set)
+import qualified Data.Set as Set
 
-type HandlerFunc = SockAddr -> String -> IO ()
+type HandlerFunc = SockAddr -> String -> CountingStateMonad ()
 
 -- serveLogState :: String
 --               -> HandlerFunc
@@ -16,55 +19,65 @@ type HandlerFunc = SockAddr -> String -> IO ()
 
 serveLog :: String              -- ^ Port number or name; 514 is default
          -> HandlerFunc         -- ^ Function to handle incoming messages
-         -> IO ()
-serveLog port handlerfunc = withSocketsDo $
-    do -- Look up the port.  Either raises an exception or returns
-       -- a nonempty list.  
-       addrinfos <- getAddrInfo 
-                    (Just (defaultHints {addrFlags = [AI_PASSIVE]}))
-                    Nothing (Just port)
-       let serveraddr = head addrinfos
+         -> CountingStateMonad ()
+serveLog port handlerfunc =
+    --do
+    --  h <- helper -- m a <- t m a
+    --  r <- lift $ withSocketsDo h  -- (m a -> t m a) $ (m a -> m a) (m a)
+    --  return r
+    --helper >>= \h ->
+    --lift $ withSocketsDo h
 
-       -- Create a socket
-       sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
+    helper
+    where
+      helper :: CountingStateMonad () -- t m a
+      helper = do -- Look up the port.  Either raises an exception or returns
+         -- a nonempty list.
+         addrinfos <- lift $ getAddrInfo
+                      (Just (defaultHints {addrFlags = [AI_PASSIVE]}))
+                      Nothing (Just port)
+         let serveraddr = head addrinfos
 
-       -- Bind it to the address we're listening to
-       bindSocket sock (addrAddress serveraddr)
+         -- Create a socket
+         sock <- lift $ socket (addrFamily serveraddr) Datagram defaultProtocol
 
-       -- Loop forever processing incoming data.  Ctrl-C to abort.
-       procMessages sock
-    where procMessages sock =
-              do -- Receive one UDP packet, maximum length 1024 bytes,
-                 -- and save its content into msg and its source
-                 -- IP and port into addr
-                 (msg, _, addr) <- recvFrom sock 1024
-                 -- Handle it
-                 handlerfunc addr msg
-                 -- And process more messages
-                 procMessages sock
+         -- Bind it to the address we're listening to
+         lift $ bindSocket sock (addrAddress serveraddr)
+
+         -- Loop forever processing incoming data.  Ctrl-C to abort.
+         procMessages sock
+        where procMessages sock =
+                do -- Receive one UDP packet, maximum length 1024 bytes,
+                   -- and save its content into msg and its source
+                   -- IP and port into addr
+                   (msg, _, addr) <- lift (recvFrom sock 1024)
+                   -- Handle it
+                   handlerfunc addr msg
+                   -- And process more messages
+                   procMessages sock
 
 -- A simple handler that prints incoming packets
 plainHandler :: HandlerFunc
-plainHandler addr msg = 
-    putStrLn $ "From " ++ show addr ++ ": " ++ msg
+plainHandler addr msg = do
+    tell $ Set.singleton $ "From " ++ show addr ++ ": " ++ msg
 
-type CountingStateMonad = State Int
-increment :: CountingStateMonad Int
+type CountingStateMonad = RWST Int (Set String) Int IO
+increment :: CountingStateMonad ()
 increment = do
     count <- get
     put $ count + 1
-    return count
-
-incrementAndGet :: CountingStateMonad Int
-incrementAndGet = do
-    count <- increment
-    count <- increment
-    return count
 
 -- type HandlerFunc = SockAddr -> String -> IO ()
 countingHandler :: HandlerFunc
 countingHandler addr msg = do
-    putStrLn $ show $ evalState incrementAndGet 0
+    num <- get
+    tell $ Set.singleton ((show num) ++ msg)
+    increment
 
-main = serveLog "1514" countingHandler
+
+main =
+  let thing = serveLog "1514" countingHandler in
+  do
+    result <- runRWST thing 0 0
+    print $ show $ result
 -- main = print (evalState (serveLogState "1514" countingHandler) 0)
