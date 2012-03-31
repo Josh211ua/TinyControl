72959843c51ffef051ade3b66c0404193db72c4f
Joshua Smith <jsmit272@jhu.edu>
2012-03-31 16:11:09 -0400

    Framework for server

---
+++ serverlib.hs
@@ -0,0 +1,63 @@
+module TinyControl.Server
+  ( Data(..)
+  , Handle
+  , open
+  , recv
+  , send
+  , close
+  ) where
+
+import Data.ByteString (ByteString)
+import Network.Socket (Socket, SockAddr, getAddrInfo, socket)
+import Network.BSD (HostName)
+import System.Time (TimeDiff, CalendarTime)
+import Data.Set (Set)
+
+type Data = [ByteString]
+
+data Addr = Addr { sock :: Socket
+                 , address :: SockAddr
+                 }
+
+data ServerState = ServerState { rto :: TimeDiff
+                               , tld :: CalendarTime
+                               , r :: TimeDiff
+                               , x_recvset :: Set (CalendarTime, Int)
+                               , x :: Int
+                               }
+
+data Handle = Handle { addr :: Addr
+                     , state :: ServerState
+                     }
+
+-- Hostname -> Port -> Handle
+open :: HostName -> String -> IO Handle
+open = error "open not implemented"
+open hostname port =
+    do -- Look up the hostname and port.  Either raises an exception
+       -- or returns a nonempty list.  First element in that list
+       -- is supposed to be the best option.
+       addrinfos <- getAddrInfo Nothing (Just hostname) (Just port)
+       let serveraddr = head addrinfos
+
+       -- Establish a socket for communication
+       theSock <- socket (addrFamily serveraddr) Datagram defaultProtocol
+
+       -- Save information into an Addr
+       let theAddr = { sock=theSock, address=serveraddr }
+
+       -- Initailize State
+       let theState = -- TODO
+
+       -- Send back the handle
+       return $ Handle { addr=theAddr, state=theState }
+
+
+recv :: Handle -> IO (Handle, Data)
+recv handle = error "recv not implemented"
+
+send :: Handle -> Data -> IO (Handle)
+send handle msg = error "send not implemented"
+
+close :: Handle -> IO ()
+close handle = error "close not implemented"
