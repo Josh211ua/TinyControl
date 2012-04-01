0ea33ffe0a2dabc73b1b7a39286ec401cb37d9cd
Joshua Smith <jsmit272@jhu.edu>
2012-04-01 14:33:49 -0400

    Server App serves multiple clients using threads

--- ServerApp.hs
+++ ServerApp.hs
@@ -1,14 +1,25 @@
 import TinyControl.Server
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 (pack, unpack)
+import Control.Concurrent(myThreadId, forkIO)
 
-main =
-  do
-    h <- open "1514"
-    (handle, friend, msg) <- srecv h
-    putStrLn ("Received: " ++ (unpack msg))
+serverThread :: ServerHandle -> Friend -> Data -> IO ()
+serverThread handle friend msg = do
+    myId <- myThreadId
+    putStrLn (show myId ++ ": Received: " ++ (unpack msg))
     handle <- send handle friend (pack "hello")
     (handle, friend, msg) <- recv handle
-    putStrLn ("Received: " ++ (unpack msg))
+    putStrLn (show myId ++ ": Received: " ++ (unpack msg))
     handle <- send handle friend (pack "goodbye")
-    close h
+    close handle
+
+serve :: ServerHandle -> IO ()
+serve h = do
+    (handle, friend, msg) <- srecv h
+    threadId <- forkIO (serverThread handle friend msg)
+    serve h
+
+main :: IO ()
+main = do
+    h <- open "1514"
+    serve h
