fcac58a7408ffe0a280c012712e171727be61894
Joshua Smith <jsmit272@jhu.edu>
2012-04-03 17:48:51 -0400

    Client framework

--- TinyControl/Client.hs
+++ TinyControl/Client.hs
@@ -45,14 +45,40 @@ type ClientStateMonad r = RWST r [String] ClientState IO
 wantData :: String -> String -> Data -> IO Data
 wantData host port msg = do
     x <- open host port
-    let (handle, friend) = x
-    handle <- send handle friend msg
-    recvAll handle friend
+    let (h@(Handle{sock = s, state = ss}), friend) = x
+    send s friend msg
+    recvAll ( h ) friend
 
 recvAll :: ClientHandle -> Friend -> IO Data
-recvAll handle friend = error "recvAll not initialized"
+recvAll (Handle {sock = s, state = ss}) friend = do
+    (d, _, _) <- runRWST (m1 (timeout 1 (recv s))) (s, friend) ss
+    return d
 
-
+m1 :: IO (Maybe (Friend, Data)) -> ClientStateMonad (Socket, Friend) (Data)
+m1 result = do
+    d <- lift result
+    error "fail"
+        
+--    m1 (handle, friend)
+--    where
+--        m1 :: ClientStateMonad (Socket, Friend)
+--        m1 = do
+--            (handle, friend) <- ask
+--            maybe <- timeout (undefined) $ recv handle
+--            case maybe of
+--                 Just(h, f, msg) -> -- doSomething
+--                    m2
+--                 None -> -- doSomething
+--                    m3
+--        m2 :: ClientStateMonad (Socket, Friend)
+--        m2 = do
+--
+--        m3 :: ClientStateMonad (Socket, Friend)
+--        m3 = do
+--            (handle, friend) <- ask
+--            (h, f, msg) <- recv handle
+--            -- Do Something
+--            m1 (h, f)
 
 open :: String -> String -> IO (Handle ClientState, Friend)
 open hostname port =
@@ -80,26 +106,35 @@ open hostname port =
        -- Send back the handle
        return $ (Handle { sock=theSock, state=theState }, addrAddress serveraddr)
 
-recieveHelper :: ClientStateMonad (Socket) (Friend, Data) -- t m a
-recieveHelper = do
-  sock <- ask
-  -- Receive one UDP packet, maximum length 1024 bytes,
-  -- and save its content into msg and its source
-  -- IP and port into addr
-  (msg, _, addr) <- lift (recvFrom sock 1024)
-  let d = (pack msg)
-  return (addr, d)
+recv :: Socket -> IO (Friend, Data)
+recv sock = do
+    (msg, _, addr) <- recvFrom sock 1024
+    let d = (pack msg)
+    return (addr, d)
 
-recv :: Handle ClientState -> IO (Handle ClientState, Friend, Data)
-recv h = C.recv h recieveHelper
+-- recieveHelper :: ClientStateMonad (Socket) (Friend, Data) -- t m a
+-- recieveHelper = do
+--   sock <- ask
+--   -- Receive one UDP packet, maximum length 1024 bytes,
+--   -- and save its content into msg and its source
+--   -- IP and port into addr
+--   (msg, _, addr) <- lift (recvFrom sock 1024)
+--   let d = (pack msg)
+--   return (addr, d)
+-- 
+-- recv :: Handle ClientState -> IO (Handle ClientState, Friend, Data)
+-- recv h = C.recv h recieveHelper
 
-sendHelper :: ClientStateMonad (Socket,Friend,Data) ()
-sendHelper = do
-    (sock, friend, msg) <- ask
-    lift $ C.sendstr sock friend (unpack msg)
+send :: Socket -> Friend -> Data -> IO ()
+send sock friend msg = C.sendstr sock friend (unpack msg)
 
-send :: Handle ClientState -> Friend -> Data -> IO (Handle ClientState)
-send h friend msg = C.send h friend msg sendHelper
+-- sendHelper :: ClientStateMonad (Socket,Friend,Data) ()
+-- sendHelper = do
+--     (sock, friend, msg) <- ask
+--     lift $ C.sendstr sock friend (unpack msg)
+-- 
+-- send :: Handle ClientState -> Friend -> Data -> IO (Handle ClientState)
+-- send h friend msg = C.send h friend msg sendHelper
 
 close :: Handle ClientState -> IO ()
 close = C.close
