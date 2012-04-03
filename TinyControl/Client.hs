821adbfdbd6258e0141c7be5edc51483c1545f13
Leah Hanson <astrieanna@gmail.com>
2012-04-03 15:44:37 -0400

    shortened ClientApp

--- TinyControl/Client.hs
+++ TinyControl/Client.hs
@@ -44,8 +44,7 @@ type ClientStateMonad r = RWST r [String] ClientState IO
 
 wantData :: String -> String -> Data -> IO Data
 wantData host port msg = do
-    x <- open host port
-    let (handle, friend) = x
+    (handle, friend) <- open host port
     handle <- send handle friend msg
     recvAll handle friend
 
