cf8fafb6bc554a8bcd3434529227e02d4a745dfc
Leah Hanson <astrieanna@gmail.com>
2012-04-05 20:14:11 -0400

    converted TinyControl/Server to use the ServerStateMonad more effectively...

--- TinyControl/Server.hs
+++ TinyControl/Server.hs
@@ -52,40 +52,50 @@ data ServerState = ServerState { rto :: NominalDiffTime
 
 type ServerHandle = Handle ServerState
 
-type ServerStateMonad r = RWST r [String] ServerState IO
+type ServerStateMonad = RWST (Socket,Friend) [String] ServerState IO
 type HandlerFunction = Data -> IO Data
+
 
 serveData :: String -> HandlerFunction -> IO ()
 serveData port f = do
   h <- open port
   serve h
   where
-    serve :: ServerHandle -> IO ()
+    serve :: Socket -> IO ()
     serve h = do
       (handle, friend, msg) <- srecv h
       threadId <- forkIO (serverThread handle friend msg)
       serve h
-    serverThread :: ServerHandle -> Friend -> Data -> IO ()
-    serverThread handle friend msg = do
+    serverThread :: Socket -> Friend -> Data -> IO ()
+    serverThread sock friend msg = do
       resp <- f msg
-      serverThreadHelper handle friend resp
-      close handle
-    serverThreadHelper :: ServerHandle -> Friend -> Data -> IO ()
-    serverThreadHelper _ _ m | (ByteString.length m) == 0 = (trace "done") return ()
-    serverThreadHelper handle friend msg = do
-      let (mmsg, rest) = ByteString.splitAt P.s msg
       now <- T.now
+      let theState = ServerState {
+                  rto = T.sToDiffTime 2,
+                  tld = now,
+                  r = Nothing,
+                  x_recvset = Set.empty,
+                  x = P.s
+                  }
+      (a,s,w) <- runRWST (serverThreadHelper resp) (sock,friend) theState
+      sClose sock
+    serverThreadHelper :: Data -> ServerStateMonad ()
+    serverThreadHelper m | (ByteString.length m) == 0 = (trace "done") return ()
+    serverThreadHelper msg = do
+      (sock, friend) <- ask
+      let (mmsg, rest) = ByteString.splitAt P.s msg
+      now <- lift T.now
       let rmsg = P.DataPacket {
           P.seqNum = 0,
           P.timeStamp = now,
           P.rtt = 2,
           P.payload = mmsg
           }
-      handle <- send handle friend (show rmsg)
-      (serverThreadHelper handle friend rest)
+      lift $ send sock friend (show rmsg)
+      (serverThreadHelper rest)
 
 
-open :: String -> IO (ServerHandle)
+open :: String -> IO (Socket)
 open port =
     do -- Look up the hostname and port.  Either raises an exception
        -- or returns a nonempty list.  First element in that list
@@ -97,76 +107,51 @@ open port =
 
        -- Establish a socket for communication
        theSock <- socket (addrFamily serveraddr) Datagram defaultProtocol
-       --let friend = serveraddr
        bindSocket theSock (addrAddress serveraddr)
-       -- Initailize State
-       now <- T.now
-       let theState = ServerState {
-            rto = T.sToDiffTime 2,
-            tld = now,
-            r = Nothing,
-            x_recvset = Set.empty,
-            x = P.s
-            }
 
        -- Send back the handle
-       return $ Handle { sock=theSock, state=theState }
+       return theSock
 
 
-srecv :: ServerHandle -> IO (ServerHandle, Friend, Data)
-srecv h@(Handle {sock = s , state = ss}) =
+srecv :: Socket -> IO (Socket, Friend, Data)
+srecv s =
   withSocketsDo $
   do
-    result <- runRWST receiveHelper s ss
-    let ((friend, val), state,_) = result
-    --addrinfos <- getAddrInfo (Just (defaultHints {addrFlags = [AI_PASSIVE]})) Nothing Nothing
+    result <- receiveHelper s
+    let (friend, val) = result
+
     (host, service) <- getNameInfo [] True True friend
     addrinfos <- getAddrInfo (Just defaultHints) host service
     let serveraddr = head addrinfos
-     -- Establish a socket for communication
     sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
-    return $ (Handle {sock = sock, state = state}, friend, val)
-    -- Change addr to a new port
+
+    return $ (sock, friend, val)
 
   where
-    receiveHelper :: ServerStateMonad (Socket) (Friend, Data) -- t m a
-    receiveHelper = do
-      sock <- ask
+    receiveHelper :: Socket -> IO (Friend, Data) -- t m a
+    receiveHelper sock = do
       -- Receive one UDP packet, maximum length 1024 bytes,
       -- and save its content into msg and its source
       -- IP and port into addr
-      (msg, num, addr) <- lift (recvFrom sock 1024)
-      tell (["Recv'd: " ++ msg])
+      (msg, num, addr) <- (recvFrom sock 1024)
+      --tell (["Srecv'd: " ++ msg])
       let d = (pack msg)
       return (addr, d)
 
 
-recv :: ServerHandle -> IO (ServerHandle, Friend, Data)
-recv h@(Handle {sock = a, state = ss}) =   withSocketsDo $
-  do
-    ((friend, val), state,_) <- runRWST receiveHelper a ss
-    return $ (Handle {sock = a, state = state}, friend, val)
-
-  where
-    receiveHelper :: ServerStateMonad (Socket) (Friend, Data) -- t m a
-    receiveHelper = do
-      sock <- ask
+recv :: ServerStateMonad (P.FeedbackPacket)
+recv = do
+    (sock, friend) <- ask
       -- Receive one UDP packet, maximum length 1024 bytes,
       -- and save its content into msg and its source
       -- IP and port into addr
-      mresult <- lift $ timeout 5 (recvFrom sock 1024)
-      case mresult of
-        Nothing -> error "timer expired"
-        Just (msg, num, addr) -> do
-          tell (["Recv'd: " ++ msg])
-          let d = (pack msg)
-          return (addr, d)
+    mresult <- lift $ timeout 5 (recvFrom sock 1024)
+    case mresult of
+      Nothing -> error "timer expired"
+      Just (msg, _, _) -> do
+        tell (["Recv'd: " ++ msg])
+        return $ read msg
 
+send :: Socket -> Friend -> String -> IO ()
+send a friend msg = C.sendstr a friend msg
 
-send :: ServerHandle -> Friend -> String -> IO (ServerHandle)
-send h@(Handle {sock = a, state = ss}) friend msg = do
-  C.sendstr a friend msg
-  return h
-
-close :: ServerHandle -> IO ()
-close (Handle {sock = s , state = _}) = sClose (s)
